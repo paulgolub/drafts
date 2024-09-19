34d2bb5863f8b996b6b8e190969bb0cc53cabe62
paulgolub <30668256+paulgolub@users.noreply.github.com>
2024-09-19 15:48:40 +0200

    Update last-linux-entrance.sh

--- last-linux-entrance.sh
+++ last-linux-entrance.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-# last entrance to linux
+# last login to linux
+# chmod +x last_login.sh 
 
-last_login=$(last -1 -R | awk '{print $4, $5, $6, $7}')
-echo "Last entrance: $last_login"
+last_login_info=$(last -1)
+
+user=$(echo "$last_login_info" | awk '{print $1}')
+ip=$(echo "$last_login_info" | awk '{print $3}')
+datetime=$(echo "$last_login_info" | awk '{print $4, $5, $6, $7}')
+
+echo "User: $user"
+echo "IP: $ip"
+echo "Date time: $datetime"
